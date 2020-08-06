6e0ed5b72d2bfa56e4d6fd5353fdb8a5eeb48c12
Tarek Ali <tarekali15@outlook.com>
2020-08-06 10:34:07 -0500

    fixed bug/mistake with deserialize

--- deserialize_yaml.py
+++ deserialize_yaml.py
@@ -1,6 +1,7 @@
 import yaml
 
+
 def deserialize(dict_key):
     with open('config.yaml') as file:
         key_list = yaml.load(file, Loader=yaml.FullLoader)
-        return key_list['dict_key']
+        return key_list[dict_key]
