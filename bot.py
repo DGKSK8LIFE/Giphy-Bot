0ae0b989ceb1c2b0473c531f3a01e766c59929ca
Tarek Ali <tarekali15@outlook.com>
2020-08-06 10:34:39 -0500

    fixed bug with command

--- bot.py
+++ bot.py
@@ -3,22 +3,26 @@ from discord.ext import commands
 from giphy import giphy
 from deserialize_yaml import deserialize
 
-client = commands.Bot(command_prefix = '!')
+client = commands.Bot(command_prefix='!')
+
 
 @client.event
 async def on_ready():
     print(f'Giphybot ready to assist!')
 
+
 @client.event
 async def on_member_join(member):
     await member.send(f'Giphybot welcomes {member.display_name}.')
+
 
 @client.event
 async def on_member_remove(member):
     print(f'Giphybot is sad to see {member.display_name} go.')
 
-@client.command 
-def display_gif(ctx, tag, rating):
+
+@client.command
+async def display_gif(ctx, tag, rating):
     await ctx.send(giphy(tag, rating))
 
 client.run(deserialize('bot-token'))
