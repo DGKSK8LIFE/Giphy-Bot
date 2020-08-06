05d63a1266ab50e5f9e68df7e5d2d032050a9961
Tarek Ali <tarekali15@outlook.com>
2020-08-05 19:50:53 -0500

    simplified bot so I can improve it

--- bot.py
+++ bot.py
@@ -17,35 +17,4 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print("A member has left.")
 
-@client.command()
-async def help(ctx):
-    await ctx.send("""                              
-        Commands:
-        !help
-        !cake
-        !vanilla
-        !chocolate
-        !strawberry
-                            
-                            
-                            
-                        """)
-
-@client.command()
-async def cake(ctx):
-    await ctx.send(embed = discord.Embed().set_image(url = giphy('cake')))
-
-@client.command()
-async def vanilla(ctx):
-    await ctx.send(embed = discord.Embed().set_image(url = giphy('vanilla cake')))
-
-@client.command()
-async def chocolate(ctx):
-    await ctx.send(embed = discord.Embed().set_image(url = giphy('chocolate cake')))
-
-@client.command()
-async def strawberry(ctx):
-    await ctx.send(embed = discord.Embed().set_image(url = giphy('strawberry cake')))
-
-
 client.run(deserialize('bot-token'))
